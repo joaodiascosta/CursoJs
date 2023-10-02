c92b4e713fca651450d400a1a19370676c884e37
joaodiascosta <joaodiasdncosta@gmail.com>
2023-10-02 14:36:33 -0300

    exercicios completos

---
+++ aula14/script.js
@@ -0,0 +1,16 @@
+/* var c = 1
+while (c <= 10) {
+    bloco
+    c++
+} */
+ 
+/* do {
+    bloco 
+    c++
+} while (c <= 10) */
+
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(`Passo um ${c}`)
+}
+console.log('FIM!')
