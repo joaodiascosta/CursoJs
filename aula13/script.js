c436b565cb2c9a331bec73e956183dffc93d22b4
joaodiascosta <joaodiasdncosta@gmail.com>
2023-10-02 12:34:02 -0300

    aula 13

---
+++ aula13/script.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`Passo um ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+/*
+var c = 1
+while ( c <= 3) {
+    console.log(`Passo um ${c}`)
+    c++ // c = c + 1
+}
+*/
